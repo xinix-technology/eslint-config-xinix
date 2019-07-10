d0027b84a9b719f3ac3233a29f4f26a48263f24e
Ganesha <reekoheek@gmail.com>
2019-07-10 17:34:08 +0700

    Upgrade deps

---
+++ eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'standard',
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-var': ['error'],
+    'no-console': ['error', { 'allow': ['info', 'warn', 'error'] }],
+    'require-await': ['error'],
+  },
+  env: {
+    browser: true,
+    node: true,
+    mocha: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
